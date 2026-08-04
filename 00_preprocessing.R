9a4e8dac5f70c062c67847407eb821922c409da4
Riley <rileyjbutler@gmail.com>
2026-08-04 14:17:19 +1200

    Starting pre-processing in 00, loading libraries, environment set-up and retrieving data.

---
+++ 00_preprocessing.R
@@ -0,0 +1,21 @@
+# This file is used to retrieve and process the data for the WGCNA. 
+# The following script is adopted from hadiazarabad's repository 'Cancer-Drug-Response' from file 'prepare_gse25066.R' 
+
+# load libraries 
+library(GEOquery)
+library(limma)
+library(dplyr)
+
+# environment set-up - make data directory and output file path
+data_dir <- "data" 
+if (!dir.exists(data_dir)) { 
+  dir.create(data_dir, recursive=TRUE)
+}
+
+output_file <- file.path(data_dir, "processed_data.rds")
+
+# retrieve data 
+gse <- getGEO("GSE25066", GSEMatrix=TRUE)
+
+
+
