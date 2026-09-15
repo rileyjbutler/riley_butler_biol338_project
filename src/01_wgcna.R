72c2bd4bced9875497685488de92d65a6ea93356
Riley <rileyjbutler@gmail.com>
2026-09-16 09:34:03 +1200

    committing new folders

---
+++ src/01_wgcna.R
@@ -0,0 +1,192 @@
+# This file is used to carry out a Weighted Gene Co-expression Networks Analysis using the processed data from 00_preprocessing
+
+# load libraries 
+library("WGCNA")
+library(tidyverse)
+library(pheatmap)
+
+data_dir <- "data" 
+if (!dir.exists(data_dir)) { 
+  dir.create(data_dir, recursive=TRUE)
+}
+
+processed_path <- "data/processed_data.rds"
+output_file <- file.path(data_dir, "wgcna_results.rds")
+
+# retrieving processed data 
+processed <- readRDS(processed_path)
+expression <- processed$expression_raw
+response <- factor(processed$response, levels = c("RD", "pCR"))
+genes <- processed$genes
+pheno <- processed$phenotype
+
+# identifying outlier samples 
+sampleTree <- hclust(dist(expression), method="average")
+
+par(cex=0.6);
+par(mar=c(0,4,2,0))
+
+plot(sampleTree, main="Sample Clustering to Detect Outliers", sub="", xlab="", cex.lab=1.5, cex.axis=1.5, cex.main=2)
+# there appears to be no clear outlier samples
+
+# choosing soft power threshold 
+spt <- pickSoftThreshold(expression)
+
+# plotting soft power thresholds
+par(mar=c(1,1,1,1))
+plot(spt$fitIndices[,1],spt$fitIndices[,2],
+xlab="Soft Threshold", ylab="Scale Free Topology Model Fit", type="n", 
+main=paste("Scale independece"))
+text(spt$fitIndices[,1], spt$fitIndices[,2], col="red")
+abline(h=0.80, col="red")
+
+# plotting mean connectivity
+par(mar=c(5,5,3,1))
+plot(spt$fitIndices[,1], spt$fitIndices[,5],
+     xlab="Soft Threshold", ylab="Mean Connectivity", type="n", xaxt="n", 
+     main=paste("Mean connectivity"))
+axis(side=1, at=spt$fitIndices[,1], labels=spt$fitIndices[,1])
+text(spt$fitIndices[,1], spt$fitIndices[,5], labels=spt$fitIndices[,1], col="red")
+
+# choose soft power = 3
+# CHANGE FOR VALIDATION SET (add if/else)
+SoftPower <- 3
+
+# construct adjacency matrix
+adjacency <- adjacency(expression, power=SoftPower)
+
+# calculate TOM dissimilarity
+TOM <- TOMsimilarity(adjacency)
+TOM_dis <- 1-TOM
+
+# plot gene tree
+geneTree <- hclust(as.dist(TOM_dis), method='average')
+
+sizeGrWindow(12,9)
+plot(geneTree, xlab='', sub='', main="Gene clustering on TOM-based dissimilarity", 
+     labels=FALSE, hang=0.04)
+
+# find modules and assign module colours
+Modules <- cutreeDynamic(dendro=geneTree, distM=TOM_dis, deepSplit=2, pamRespectsDendro=FALSE, minClusterSize=30)
+table(Modules)
+
+ModuleColors <- labels2colors(Modules)
+table(ModuleColors)
+
+# plot dendrogram with module colours
+plotDendroAndColors(geneTree, ModuleColors, "Module", 
+                    dendroLabels=FALSE, hang=0.03, 
+                    addGuide=TRUE, guideHange=0.05, 
+                    main="Gene Dendrogram and Module Colours")
+
+# module eigengene identification 
+MElist <- moduleEigengenes(expression, colors=ModuleColors)
+MEs <- MElist$eigengenes 
+
+# module merging 
+ME_dis <- 1-cor(MElist$eigengenes, use="complete") # calculate eigengene dissimilarity 
+METree <- hclust(as.dist(ME_dis), method="average") # clustering eigengenes 
+par(mar=c(0,4,2,0))
+par(cex=0.6)
+plot(METree)
+abline(h=.25, col="red")
+
+merge <- mergeCloseModules(expression, ModuleColors, cutHeight=0.25)
+
+mergedColors <- merge$colors
+mergedMEs = merge$newMEs
+
+# plot merged vs unmerged modules for comparison 
+plotDendroAndColors(geneTree, cbind(ModuleColors, mergedColors), 
+                    c("Original Modules", "Merged Modules"),
+                    dendroLabels = FALSE, hang = 0.03,
+                    addGuide = TRUE, guideHang = 0.05,
+                    main = "Gene Dendrogram and Module Colors for Original and Merged Modules")
+
+# module-trait matching
+
+# matching trait samples to expression samples 
+common <- intersect(rownames(expression), pheno$geo_accession)
+expression2 <- expression[common, , drop=FALSE]
+datatraits <- pheno[match(common, pheno$geo_accession), , drop=FALSE]
+rownames(datatraits) <- datatraits$geo_accession
+
+
+# matching with samples from metadata with MEs
+common2 <- intersect(rownames(mergedMEs), rownames(datatraits))
+MEs2 <- mergedMEs[common2, , drop = FALSE]
+datatraits2 <- datatraits[common2, , drop = FALSE]
+identical(rownames(MEs2), rownames(datatraits2))
+
+# calculating module-trait correlation
+
+nGenes <- ncol(expression2) # 10000
+nSamples <- nrow(expression2) # 306
+
+traits_numeric <- datatraits2[, setdiff(names(datatraits2), "geo_accession")] # get rid of character variable
+module_trait_corr <- WGCNA::cor(MEs2, traits_numeric, use='p')
+module_trait_pvalue <- corPvalueStudent(module_trait_corr, nSamples)
+
+# forming a heatmap 
+textMatrix = paste(signif(module_trait_corr, 2), "\n(",
+                   signif(module_trait_pvalue, 1), ")", sep = "");
+dim(textMatrix) = dim(module_trait_corr)
+par(mar = c(6, 8.5, 3, 1))
+
+
+# display the correlation values within a heatmap plot
+labeledHeatmap(Matrix = module_trait_corr,
+               xLabels = names(traits_numeric),
+               yLabels = names(MEs2),
+               ySymbols = names(MEs2),
+               colorLabels = FALSE,
+               colors = blueWhiteRed(50),
+               textMatrix = textMatrix,
+               setStdMargins = FALSE,
+               cex.text = 0.4,
+               zlim = c(-1,1),
+               main = paste("Module-trait relationships"))
+
+
+# exploration for module eigengenes 
+
+queryModuleColor <- "blue" # using blue module 
+
+# get expression and traits data for only the selected module colour 
+queryModuleExpression <- expression[,which(ModuleColors==queryModuleColor)]
+queryModuleExpression2 <- queryModuleExpression[rownames(datatraits2), , drop = FALSE]
+
+# a heatmap of sample clustering for genes inside the blue module based on their association with pCR (0 or 1)
+heatmap1 <- heatmap(as.matrix(queryModuleExpression2), RowSideColors=c("red", "black")[as.numeric(as.factor(datatraits2$pathologic_response))])
+
+# combining pathologic response and ER status
+annotation_row <- data.frame(Response = datatraits$pathologic_response, ER_status = factor(datatraits$ER_status, levels = factor(c(0, 1)), labels = c("ER-", "ER+")))
+
+rownames(annotation_row) <- rownames(datatraits)
+
+# heat map of the blue module 
+pheatmap(queryModuleExpression, scale = "column", annotation_row = annotation_row, show_rownames = FALSE, main = "Blue Module Gene Correlation with ER Status and Chemotherapy Response")
+
+# ER status is shown in red and blue, showing a distinct cluster for ER+ (red) and ER- (blue) in the yellow module 
+# pCR shown in green/white, showing no distinct clustering in the yellow module
+
+# view as PCA 
+queryModulePCA<-prcomp(queryModuleExpression,scale=TRUE,center=TRUE)
+
+summary(queryModulePCA)$importance[,2]
+
+# plot - seems to have roughly equal split in direction across PC1 and PC2
+biplot(queryModulePCA, cex=0.4)
+
+# find Eigengene 
+Eigengene1 <- MEs[,which(colnames(MEs)==paste0("ME", queryModuleColor))]
+
+# find first principle component
+PC1 <- queryModulePCA$x[,1]
+
+# plot - some separation of pCR/RD cases, lots more separation for ER status 
+plot(Eigengene1, PC1, col=c("red", "black")[as.factor(pheno$pathologic_response)])
+plot(Eigengene1, PC1, col=c("red", "black")[as.factor(pheno$ER_status)])
+
+wgcna_results <- list(eigengenes=MEs2, pheno=pheno, ModuleColors=mergedColors)
+saveRDS(wgcna_results, output_file)
---
+++ src/02_model_selection.R
@@ -0,0 +1,176 @@
+# This file is used to explore different model types for predicting pCR. 
+# The chosen model is the one with the best balance of specificity, sensitivity and AUC as well as the model that best aligns with the data contextually.
+
+# script set-up 
+if (!dir.exists("results")) {
+  dir.create("results", recursive = TRUE)
+}
+
+wgcna_path <- "data/wgcna_results.rds"
+if (!file.exists(wgcna_path)) {
+  system("Rscript 01_wgcna.R")
+}
+
+output_file <- file.path(data_dir, "models.rds")
+
+# load libraries 
+library(tidyverse)
+library(performance)
+library(glmnet)
+library(caret)
+library(pROC)
+
+# load data
+data <- readRDS(wgcna_path)
+
+clinical_factors <- data$pheno
+eigengenes <- data$eigengenes
+
+# dropping predictors that are too related to the outcome (pathologic response)
+clinical_factors <- clinical_factors |> select(-geo_accession, -drfs, -grade, -erbb2_status, -indeterminate_ER_status)
+
+# create dataframe
+model_data <- data.frame(clinical_factors,eigengenes)
+
+# changing outcome to be factor
+model_data$pathologic_response <- factor(model_data$pathologic_response, levels = c(1, 0), labels = c("pCR", "RD"))
+
+# formatting for the model to be trained against 
+control <- caret::trainControl(method = "repeatedcv", # uses repeated cross-validation with GSE25055 training set (model_data)
+                               number = 5, # number of folds 
+                               repeats = 5, # number of repeated folds
+                               verboseIter = TRUE, # print training log 
+                               classProbs = TRUE, # calculated class probabilities for each re-sample 
+                               summaryFunction = twoClassSummary, # to compute performance metrics 
+                               savePredictions = "final") # indicator of how much of the hold-out predictions for each resample should be saved 
+
+
+# ELASTIC NET LOGISTIC REGRESSION # 
+# using caret to choose parameters (alpha and lambda) and fit elastic net logistic regression model 
+
+# specifying range of alpha and lambda values to try. For reproducibility. 
+grid <- expand.grid(alpha = seq(0, 1, by = 0.1), lambda = 10^seq(-4, 0, length.out = 50))
+
+# constructing the elastic net logistic regression model
+set.seed(123)
+
+# running the elastic model 
+elastic_model <- caret::train(pathologic_response ~ .,
+                      data = model_data,
+                      method = "glmnet", # using elastic-net logistic regression 
+                      preProcess = c("center", "scale"),
+                      tuneGrid = grid,
+                      metric = "ROC", # use ROC as performance measure 
+                      trControl = control) # use information from the control 
+
+# LOGISTIC REGRESSION # 
+# first I will construct 3 general logistic regression models - 1 with only eigegenes, 1 with clinical factors and 1 combined model. 
+# I run some model diagnostics by checking that none of the predictors are co-linear (VIF <=10)
+
+# full model
+logistic1 <- glm(pathologic_response ~ MEblue + MEgreenyellow + MEtan + MEgreenyellow + MEblack + MEgreen + MEpink + MEsalmon + MEmagenta + MEred + MEpurple + MEturquoise + ER_status + tumor_stage  + age + PR_status + nodal_status, data=model_data, family=binomial)
+
+# ME model
+logistic2 <- glm(pathologic_response ~ MEblue + MEgreenyellow + MEtan + MEgreenyellow + MEblack + MEgreen + MEpink + MEsalmon + MEmagenta + MEred + MEpurple + MEturquoise, data=model_data, family=binomial)
+
+# clinical factor model
+logistic3 <- glm(pathologic_response ~ ER_status + tumor_stage  + age + PR_status + nodal_status, data=model_data, family=binomial)
+
+# summary statistics 
+l1 <- summary(logistic1)
+l1
+l2 <- summary(logistic2)
+l2
+l3 <- summary(logistic3)
+l3
+
+# check collinearity shows no high collinearity (VIF >= 10)
+check_collinearity(logistic1)
+check_collinearity(logistic2)
+check_collinearity(logistic3)
+
+# using caret for model comparison 
+MEpredictors <- model_data |> select(-age, -ER_status, -PR_status, -ggi_class, -HER2_status, -tumor_stage, -nodal_status, -esr1_status, -set_class)
+
+# construct logistic regression model with MEs
+set.seed(123)
+ME_logistic_model <- caret::train(pathologic_response ~ .,
+                              data = MEpredictors,
+                              method = "glm", # using logistic regression 
+                              family=binomial,
+                              metric = "ROC", # use ROC as performance measure 
+                              trControl = control)
+
+# construct logistic regression model with clinical factors
+CF_predictors <- model_data |> select(-MEblue, -MEgreenyellow, -MEtan, -MEgreenyellow, -MEblack, -MEgreen, -MEpink, -MEsalmon, -MEmagenta, -MEred, -MEpurple, -MEturquoise)
+
+set.seed(123)
+
+# running logistic regression model 
+CF_logistic_model <- caret::train(pathologic_response ~ .,
+                                  data = CF_predictors,
+                                  method = "glm", # using logistic regression 
+                                  family=binomial,
+                                  metric = "ROC", # use ROC as performance measure 
+                                  trControl = control)
+
+# construct full logistic regression model 
+set.seed(123)
+
+# running full logistic regression model 
+full_logistic_model <- caret::train(pathologic_response ~ .,
+                                  data = model_data,
+                                  method = "glm", # using logistic regression 
+                                  family=binomial,
+                                  metric = "ROC", # use ROC as performance measure
+                                  trControl = control)
+# SUPPORT VECTOR MACHINE (SVM) # 
+# SVM finds the optimal hyperplan to separate data into different classes (pCR or RD). 
+# The RBF Kernal SVM uses non-linear relationships to map the data into infinite dimensional space 
+# The tuning cost (C) defines the influence of a single training example 
+
+
+set.seed(123)
+
+# running SVM model with all predictors
+SVM_model <- caret::train(pathologic_response ~ .,
+                                    data = model_data,
+                                    method = "svmRadial", # using SVM
+                                    family=binomial,
+                                    metric = "ROC", # use ROC as performance measure 
+                                    trControl = control)
+
+# RANDOM FOREST # 
+# Random forest is an ensemble techique that builds multiple decision trees and merges their outputs to improve model accuracy and stability 
+
+set.seed(123)
+
+# running random forest with all predictors 
+random_forest_model <- caret::train(pathologic_response ~ .,
+                          data = model_data,
+                          method = "ranger", # using random forest 
+                          metric = "ROC", # use ROC as performance measure 
+                          trControl = control,
+                          importance = "permutation")
+
+# to observe the importance of each predictor - how much the model's predictive performance depends on that variable.
+importance <- varImp(random_forest_model, scale=FALSE)
+plot(importance)
+
+# comparing models using AUC, Specificity and Sensitivity 
+results <- resamples(list(glmnet = elastic_model, MElogistic = ME_logistic_model, CFlogistic = CF_logistic_model, fullLogistic = full_logistic_model, SVM_model=SVM_model, randomForest = random_forest_model))
+summary(results)
+bwplot(results)
+
+# construct R object with the models
+models <- list(
+  elastic_model=elastic_model,
+  ME_logistic_model=ME_logistic_model, 
+  CF_logistic_model=CF_logistic_model, 
+  full_logistic_model=full_logistic_model,
+  SVM_model=SVM_model, 
+  random_forest_model=random_forest_model,
+  model_data = model_data
+)
+
+saveRDS(models, output_file)
